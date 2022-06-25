c7bd2aa95fd64744d521ff96f19450d67d2f4ee5
jaejoon.han <jaejoon.han@crevisse.com>
2022-06-26 01:07:24 +0900

    search opensea testnet nft list

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import api from "api";
 
 const PORT = process.env.PORT || 9090;
 const __dirname = path.resolve();
@@ -13,3 +15,24 @@ app
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app
+  .options("/api/fetchAssets/:address", cors())
+  .get("/api/fetchAssets/:address", cors(), fetchAssets);
+
+const openseaSdk = api('@opensea/v1.0#7dtmkl3ojw4vb');
+
+async function fetchAssets(req, res, next) {
+  try {
+    let result = await openseaSdk["retrieving-assets-rinkeby"]({
+      owner: req.params.address,
+      order_direction: "desc",
+      offset: "0",
+      limit: "20",
+      include_orders: "false",
+    });
+    res.json(result);
+  } catch (e) {
+    res.status(500).send(`Internal Server Error - ${e.message}`);
+  }
+}

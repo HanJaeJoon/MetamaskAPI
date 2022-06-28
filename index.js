f2d8ea83a0ae496659ee7177137b0f53154e6a4b
jaejoon.han <jaejoon.han@crevisse.com>
2022-06-28 11:08:05 +0900

    airbnb lint 적용

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from "express";
-import path from "path";
-import cors from "cors";
-import api from "api";
-import "dotenv/config";
-import Moralis from "moralis/node.js";
-import { Network, initializeAlchemy } from "@alch/alchemy-sdk";
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import api from 'api';
+import 'dotenv/config';
+import Moralis from 'moralis/node.js';
+import { Network, initializeAlchemy } from '@alch/alchemy-sdk';
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
 
 const PORT = process.env.PORT || 9090;
 const __dirname = path.resolve();
@@ -13,54 +13,53 @@ const __dirname = path.resolve();
 const app = express();
 
 app
-  .use(express.static(path.join(__dirname, "pages")))
-  .set("views", path.join(__dirname, "views"))
-  .use(express.static(path.join(__dirname, "static")))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("index"))
+  .use(express.static(path.join(__dirname, 'pages')))
+  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join(__dirname, 'static')))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
   .use(express.json())
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+// testnet
+const openseaSdk = api('@opensea/v1.0#7dtmkl3ojw4vb');
+
 app
-  .options("/api/fetchAssets/:address", cors())
-  .get("/api/fetchAssets/:address", cors(), fetchAssets);
-
-const openseaSdk = api("@opensea/v1.0#7dtmkl3ojw4vb");
-
-async function fetchAssets(req, res, next) {
-  try {
-    let result = await openseaSdk["retrieving-assets-rinkeby"]({
-      owner: req.params.address,
-      order_direction: "desc",
-      offset: "0",
-      limit: "20",
-      include_orders: "false",
-    });
-    res.json(result);
-  } catch (e) {
-    res.status(500).send(`Internal Server Error - ${e.message}`);
-  }
-}
+  .options('/api/fetchAssets/:address', cors())
+  .get('/api/fetchAssets/:address', cors(), async (req, res) => {
+    try {
+      const result = await openseaSdk['retrieving-assets-rinkeby']({
+        owner: req.params.address,
+        order_direction: 'desc',
+        offset: '0',
+        limit: '20',
+        include_orders: 'false',
+      });
+      res.json(result);
+    } catch (e) {
+      res.status(500).send(`Internal Server Error - ${e.message}`);
+    }
+  });
 
 const serverUrl = process.env.MORALIS_APP_URL;
 const appId = process.env.MORALIS_APP_ID;
 const moralisSecret = process.env.MORALIS_KEY;
 
-app.post("/api/saveUserData", cors(), async (req, res) => {
+app.post('/api/saveUserData', cors(), async (req, res) => {
   try {
     await Moralis.start({
-      serverUrl: serverUrl,
-      appId: appId,
+      serverUrl,
+      appId,
       masterKey: moralisSecret,
     });
 
-    let body = req.body;
+    const { body } = req;
 
-    const UserAddress = Moralis.Object.extend("UserAddress");
+    const UserAddress = Moralis.Object.extend('UserAddress');
     const userAddress = new UserAddress();
 
-    userAddress.set("address", body.address);
-    userAddress.set("email", body.email);
+    userAddress.set('address', body.address);
+    userAddress.set('email', body.email);
 
     console.log(userAddress);
 
@@ -70,7 +69,7 @@ app.post("/api/saveUserData", cors(), async (req, res) => {
   }
 });
 
-app.post("/api/transferAsset", cors(), async (req, res) => {
+app.post('/api/transferAsset', cors(), async (req, res) => {
   try {
     await Moralis.enableWeb3({
       // rinkeby
@@ -79,23 +78,23 @@ app.post("/api/transferAsset", cors(), async (req, res) => {
     });
 
     await Moralis.start({
-      serverUrl: serverUrl,
-      appId: appId,
+      serverUrl,
+      appId,
       masterKey: moralisSecret,
     });
 
-    let body = req.body;
+    const { body } = req;
 
     const options = {
-      type: "erc1155",
+      type: 'erc1155',
       receiver: body.receiver,
       contractAddress: body.contractAddress,
       tokenId: body.tokenId,
       amount: body.amount,
     };
 
-    let transaction = await Moralis.transfer(options);
-    let result = await transaction.wait();
+    const transaction = await Moralis.transfer(options);
+    await transaction.wait();
   } catch (e) {
     console.log(e);
     res.status(500).send(`Internal Server Error - ${e.message}`);
@@ -110,9 +109,9 @@ app.post("/api/transferAsset", cors(), async (req, res) => {
 
 // const alchemy = initializeAlchemy(settings);
 
-async function main() {
+async function test() {
   const web3 = createAlchemyWeb3(process.env.ALCHEMY_APP_URL);
-  const contractAddress = "0x88B48F654c30e99bc2e4A1559b4Dcf1aD93FA656";
+  const contractAddress = '0x88B48F654c30e99bc2e4A1559b4Dcf1aD93FA656';
   // const etherScanUrl = `https://api-rinkeby.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${process.env.ETHERSCAN_API_KEY}`;
 
   // console.log(etherScanUrl);
@@ -144,43 +143,40 @@ async function main() {
   //   });
 
   //   return;
-  const from = "0xAAa4F83dDd55FbBeAe605d4aFd0f2C29826b58bd";
-  const to = "0x87Ae5ed8a78f81bFac2B6f028dE2d1d63fa34a41";
-  const nonce = await web3.eth.getTransactionCount(
-    from,
-    "latest"
-  );
+  const from = '0xAAa4F83dDd55FbBeAe605d4aFd0f2C29826b58bd';
+  const to = '0x87Ae5ed8a78f81bFac2B6f028dE2d1d63fa34a41';
+  const nonce = await web3.eth.getTransactionCount(from, 'latest');
 
   const transaction = {
-    nonce: nonce,
-    from: from,
-    to: to,
+    nonce,
+    from,
+    to,
     gas: 30000,
     maxPriorityFeePerGas: 1000000108,
-    contractAddress: contractAddress,
-    id: "77184660478739079778493429399302246505128367961362043519940866228197047402596",
+    contractAddress,
+    id: '77184660478739079778493429399302246505128367961362043519940866228197047402596',
   };
 
   const signedTx = await web3.eth.accounts.signTransaction(
     transaction,
-    process.env.PRIVATE_KEY
+    process.env.PRIVATE_KEY,
   );
-  
+
   console.log(signedTx.rawTransaction);
 
   web3.eth.sendSignedTransaction(
     signedTx.rawTransaction,
-    function (error, hash) {
+    (error, hash) => {
       if (!error) {
         console.log(
-          "🎉 The hash of your transaction is: ",
+          '🎉 The hash of your transaction is: ',
           hash,
-          "\n Check Alchemy's Mempool to view the status of your transaction!"
+          "\n Check Alchemy's Mempool to view the status of your transaction!",
         );
       } else {
         console.log(error);
       }
-    }
+    },
   );
 
   /*
@@ -220,4 +216,4 @@ async function main() {
     */
 }
 
-main();
+// test();

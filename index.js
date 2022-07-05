8378a659b568ca2e9fde12dbe66156bad834cf3b
jaejoon.han <jaejoon.han@crevisse.com>
2022-07-05 16:09:12 +0900

    404 redirect 방지

--- index.js
+++ index.js
@@ -379,5 +379,9 @@ app.post('/api/transferAsset', cors(), async (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).redirect('https://http.cat/404');
+  res.status(404).send(`
+    <body style="margin: 0px; background: #0e0e0e; height: 100%">
+      <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://http.cat/404">
+    </body>
+  `);
 });
